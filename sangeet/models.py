49c860ca9a5da7b84e0e2cc010e5beae4312f410
ankit <86427086+ankits-dot@users.noreply.github.com>
2023-11-30 19:40:53 +0530

    Finalised the database structure, it may change in future based on the queried data requirements

--- sangeet/models.py
+++ sangeet/models.py
@@ -1,32 +1,77 @@
 from .extensions import db
 
+#Association table for many to many relationship between Playlist and Tracks table
+track_playlist = db.Table(
+    "track_playlist",
+    db.Column("playlist_id", db.ForeignKey("playlist.id"), primary_key=True),
+    db.Column("track_id", db.ForeignKey("track.id"), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String, nullable=False, unique=True)
     first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String)
+    password = db.Column(db.String, nullable=False)
     role = db.Column(db.String, nullable=False)
+    #establish relationship with Playlist table
+    playlists = db.relationship('Playlist', back_populates='creator', cascade="all, delete")
     
-class Song(db.Model):
+class Playlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    artist = db.Column()
-    path = db.Column(db.String, nullable=False)
-    lyrics_path = db.Column(db.String, nullable=False)
-    rating = db.Column(db.String)
-    cover_image = db.Column(db.String)
+    #establish relationship with User table
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    creator = db.relationship('User', back_populates='playlists')
+    #establish relationshiop with Track table
+    tracks = db.relationship('Track', secondary=track_playlist, back_populates='belongs_to')
+
+class Creator(User):
+    artist_id = db.Column(db.Integer, primary_key=True)
+    #establish relationship with User table
+    id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    #relationship with Track table
+    tracks = db.relationship('Track', back_populates='artist')
+    #relationship with Album table
+    albums = db.relationship('Album', back_populates='artist')
+
+class FlaggedCreator(Creator):
+    pass
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    artist = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.String, nullable=False)
+    #relationship with Artist table
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+    artist = db.relationship('Artist', back_populates='albums')
+    #_______________________________
+    #relationship with Track table
+    tracks = db.relationship('Track', back_populates='album')
 
-class Playlist(db.Model):
+class Track(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    creator = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, nullable=False)
+    times_played = db.Column(db.Integer, default=0)
+    genre = db.Column(db.String)
+    rating = db.Column(db.Float)
+    #relationship with Artist table
+    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'))
+    artist = db.relationship('Artist', back_populates='tracks')
+    #relationship with Album table
+    album_id = db.Column(db.Integer, db.ForeignKey('album.id'))
+    album = db.relationship('Album', back_populates='tracks')
+    #establish relationshiop with Playlist table
+    belongs_to = db.relationship('Playlist', secondary=track_playlist, back_populates='tracks')
+    #establish relationship with Lyrics table
+    lyrics = db.association('Lyrics', cascade="all, delete")
 
-# class Association(db.Model):
-#     song_id = db.relationship(db.fro)
+class FlaggedTrack(Track):
+    pass
+
+class Lyrics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    lyrics = db.Column(db.String)
+    #establishing relationship with Track table
+    track_id = db.Column(db.String, db.ForeignKey('track.id'))
+    track = db.association('Track', back_populates=lyrics)
+
